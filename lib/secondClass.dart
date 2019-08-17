ae390283dc7590c8b3b5a2b57aed222148b5e868
muhammad rahman <rahman9600@gmail.com>
2019-08-17 22:09:30 +0700

    Update secondClass.dart
    adding string

--- lib/secondClass.dart
+++ lib/secondClass.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 
 class SecondClassLess extends StatelessWidget {
+
+  String payload;
+
+  SecondClassLess({Key key, this.payload}):super(key:key);
+
   @override
   Widget build(BuildContext context) {
     return SecondClassFul();
@@ -8,14 +13,29 @@ class SecondClassLess extends StatelessWidget {
 }
 
 class SecondClassFul extends StatefulWidget {
+
+  String payload;
+
+  SecondClassFul({this.payload});
+
   @override
   _SecondClassFulState createState() => _SecondClassFulState();
 }
 
 class _SecondClassFulState extends State<SecondClassFul> {
+
+  String payload;
+
+  _SecondClassFulState({this.payload});
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return Scaffold(
+
+      body: new Center(
+        child: new Text(
+          'Payload : $payload'
+        ),
+      ),
       
     );
   }
