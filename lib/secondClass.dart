8387280abe5ad5ba1cf81e2a9c3cd91996943b57
muhammad rahman <rahman9600@gmail.com>
2019-08-17 21:55:31 +0700

    Create secondClass.dart

---
+++ lib/secondClass.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class SecondClassLess extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SecondClassFul();
+  }
+}
+
+class SecondClassFul extends StatefulWidget {
+  @override
+  _SecondClassFulState createState() => _SecondClassFulState();
+}
+
+class _SecondClassFulState extends State<SecondClassFul> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
